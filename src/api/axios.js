888f9cb52afd3fea309d2100de5f94f58e0d8e8d
Abdul Basit <abasit6008@gmail.com>
2026-07-07 22:04:10 +0500

    Update axios base URL to production endpoint

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://127.0.0.1:8000/api/auth/",
+    baseURL: "https://full-stack-backend-ncii.onrender.com/api/auth/",
     withCredentials: true,
 });
